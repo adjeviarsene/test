3b50272815bf221cbb472c5a32f67183597b7c11
adjevianarsene <adjeviananarsene@gmail.com>
2020-07-28 17:19:02 +0300

    second

---
+++ Desktop/repo.git/branches/mytest.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+#define TEST_H
+
+ struct triangle;
+ struct triangle *new_triangel();
+ void triangle_init(struct triangle*);
+ void destroy_triangle(struct triangle*);
+ int calcul(struct traingle*);
+#endif
