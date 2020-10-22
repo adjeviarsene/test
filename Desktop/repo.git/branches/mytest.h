94abfe8d9b0460749af58c61b51520543cbafd38
Arsene <cybdry@gmail.com>
2020-10-22 16:57:55 +0300

    delete

--- Desktop/repo.git/branches/mytest.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
- struct triangle;
- struct triangle *new_triangel();
- void triangle_init(struct triangle*);
- void destroy_triangle(struct triangle*);
- int calcul(struct traingle*);
-#endif
